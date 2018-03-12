55465aa9472d74b018cd98d51a11373bc66ac6e0
Paweł Siergiejuk <pawelsiergiejuk@gmail.com>
2018-03-12 21:49:32 +0100

    Add rest of examples

--- hi_color_mode.py
+++ hi_color_mode.py
@@ -5,42 +5,34 @@
 import urwid
 
 __author__ = "Paweł Siergiejuk"
-__date__ = "08/03/2018"
+__date__ = "12/03/2018"
 __version__ = "v0.0"
 __email__ = "pawelsiergiejuk@gmail.com"
 __status__ = "Development"
 
+choices = u"Chapman Cleese Gilliam Idle Jones Palin".split()
 
-def show_or_exit(key):
-    if key in ("q", "Q"):
-        raise urwid.ExitMainLoop()
+def menu(title, choices):
+    body = [urwid.Text(title), urwid.Divider()]
+    for c in choices:
+        button = urwid.Button(c)
+        urwid.connect_signal(button, 'click', item_chosen, c)
+        body.append(urwid.AttrMap(button, None, focus_map='reversed'))
+    return urwid.ListBox(urwid.SimpleFocusListWalker(body))
 
-def main():
-    palette = [
-        ("banner", "", "", "", "#ffa", "#60d"),
-        ("streek", "", "", "", "g50",  "#60a"),
-        ("in",     "", "", "", "g38",  "#808"),
-        ("out",    "", "", "", "g27",  "#a06"),
-        ("bg",     "", "", "", "g7",   "#d06"),
-       ] 
-    
-    placeholder = urwid.SolidFill()
-    loop = urwid.MainLoop(placeholder, palette, unhandled_input=show_or_exit)
-    loop.screen.set_terminal_properties(colors=256)
-    loop.widget = urwid.AttrMap(placeholder, "bg")
-    loop.widget.original_widget = urwid.Filler(urwid.Pile([]))
+def item_chosen(button, choice):
+    response = urwid.Text([u'You chose ', choice, u'\n'])
+    done = urwid.Button(u'Ok')
+    urwid.connect_signal(done, 'click', exit_program)
+    main.original_widget = urwid.Filler(urwid.Pile([response,
+        urwid.AttrMap(done, None, focus_map='reversed')]))
 
-    div = urwid.Divider()
-    outside = urwid.AttrMap(div, "out")
-    inside = urwid.AttrMap(outside, "in")
-    txt = urwid.Text(("banner",u"Hello World)"), align="center")
-    streek = urwid.AttrMap(txt, "streek")
-    pile =  loop.widget.base_widget #this skips decorations
-    for item in (outside, inside, streek, inside, outside):
-        pile.contents.append((item, pile.options()))
-    
-    loop.run()
+def exit_program(button):
+    raise urwid.ExitMainLoop()
 
-if __name__ == "__main__":
-    main()
-
+main = urwid.Padding(menu(u'Pythons', choices), left=2, right=2)
+top = urwid.Overlay(main, urwid.SolidFill(u'\N{MEDIUM SHADE}'),
+    align='center', width=('relative', 60),
+    valign='middle', height=('relative', 60),
+    min_width=20, min_height=9)
+urwid.MainLoop(top, palette=[('reversed', 'standout', '')]).run()
---
+++ simple_menu.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+#! -*- coding:utf-8 -*-
+
+
+import urwid
+
+__author__ = "Paweł Siergiejuk"
+__date__ = "12/03/2018"
+__version__ = "v0.0"
+__email__ = "pawelsiergiejuk@gmail.com"
+__status__ = "Development"
+
+
+def show_or_exit(key):
+    if key in ("q", "Q"):
+        raise urwid.ExitMainLoop()
+
+def main():
+    palette = [
+        ("banner", "", "", "", "#ffa", "#60d"),
+        ("streek", "", "", "", "g50",  "#60a"),
+        ("in",     "", "", "", "g38",  "#808"),
+        ("out",    "", "", "", "g27",  "#a06"),
+        ("bg",     "", "", "", "g7",   "#d06"),
+       ] 
+    
+    placeholder = urwid.SolidFill()
+    loop = urwid.MainLoop(placeholder, palette, unhandled_input=show_or_exit)
+    loop.screen.set_terminal_properties(colors=256)
+    loop.widget = urwid.AttrMap(placeholder, "bg")
+    loop.widget.original_widget = urwid.Filler(urwid.Pile([]))
+
+    div = urwid.Divider()
+    outside = urwid.AttrMap(div, "out")
+    inside = urwid.AttrMap(outside, "in")
+    txt = urwid.Text(("banner",u"Hello World)"), align="center")
+    streek = urwid.AttrMap(txt, "streek")
+    pile =  loop.widget.base_widget #this skips decorations
+    for item in (outside, inside, streek, inside, outside):
+        pile.contents.append((item, pile.options()))
+    
+    loop.run()
+
+if __name__ == "__main__":
+    main()
+
